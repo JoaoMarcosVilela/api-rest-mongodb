499fef416fb3125eab444039f5a89d7f3e4554bc
JoaoMarcosVilela <joaosemp12345@gmail.com>
2024-09-04 14:10:34 -0300

    adição do get e delete

--- src/controllers/userConttroler.js
+++ src/controllers/userConttroler.js
@@ -8,6 +8,11 @@ const userConttroler = {
             const user = {
                 name: req.body.name
             }
+
+            if(user.name == ""){
+                return res.status(401).json({error: "Necessario adicionar dados!"});
+            }
+
             const response = await UserModel.create(user);
             res.status(201).json({response, msg: "Usuario cadastrado!"});
         }catch(error){
@@ -16,11 +21,20 @@ const userConttroler = {
     },
 
     read: async(req, res) => {
-        try{
-            const user = {
-                name
-            }
+        const usuarios= await UserModel.find();
+        return res.json(usuarios);
+    },
+
+    delete: async(req,res) => {
+        const { id } = req.params;
+        
+        const usuarioOnDelete = await UserModel.findOneAndDelete({ _id: id});
+
+        if(usuarioOnDelete){
+            return res.json(usuarioOnDelete);
         }
+
+        return res.status(401).json({ error: "Não foi possivel remover"});
     }
 }
 
--- src/routers/users.js
+++ src/routers/users.js
@@ -5,4 +5,12 @@ router.route("/users").post((req,res) => {
     userConttroler.create(req,res);
 });
 
+router.route("/users").get((req,res) => {
+    userConttroler.read(req,res);
+});
+
+router.route("/users/:id").delete((req,res) => {
+    userConttroler.delete(req,res);
+});
+
 module.exports = router;
